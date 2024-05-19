f09c20470770aa7655efc9b139d8d34356d7b1f9
Hazel2502 <hazelbgranados@gmail.com>
2024-05-19 14:10:15 -0600

    tep

--- next/app/niveles/nivel1/mate/page.jsx
+++ next/app/niveles/nivel1/mate/page.jsx
@@ -1,34 +1,60 @@
 import React from "react";
 import Link from 'next/link';
-import "../../../../styles/mate.css";
-import { SeparadorRosa } from "../../../../components/separador";
+import Styles from "../styles/globals.css";
+
+const buttonContainerStyle = {
+  position: 'absolute',
+  top: '48%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  zIndex: 1,
+  display: 'flex',
+  justifyContent: 'center',
+  gap: '10px',
+};
+
+const buttonStyle = {
+  flex: '1 0 auto',
+  width: '250px',
+  height: 'auto',
+};
 
 export default function MatematicaPage() {
   return (
-    <main>
-      {/* Bienvenida a mate nivel 1*/}
+    <>
       <section>
-        <SeparadorRosa/>
-        <div className="flex justify-center items-center mt-10 mb-10">
-          <img src="/img/page/starly.png" alt="Animated Image" className="h-40 w-auto mr-10 ml-10" />
-        </div>
-        <SeparadorRosa/>
+      <SeparadorRosa/>
+      <div className="flex justify-center items-center mt-10 mb-10">
+        <img src="/img/page/starly.png" alt="Animated Image" className="h-40 w-auto mr-10 ml-10" />
+      </div>
+      <SeparadorRosa/>
+      <div>
+        <NoSSR/>
+      </div>
+      <SeparadorRosa/>
+      <div className="flex justify-center items-center mt-10 mb-10">
+        <img src="/img/page/starly.png" alt="Animated Image" className="h-40 w-auto mr-10 ml-10" />
+      </div>
       </section>
 
-      {/* Botones*/}
-      <section>
-        <div className="buttonContainer">
-          <Link href="/niveles/nivel1">
-            <img src="/img/niveles/mate/geo.png" alt="Nivel 1" className="boton" />
-          </Link>
-          <Link href="/niveles/nivel2">
-            <img src="/img/niveles/mate/geo.png" alt="Nivel 2" className="boton" />
-          </Link>
-          <Link href="/niveles/nivel3">
-            <img src="/img/niveles/mate/geo.png" alt="Nivel 3" className="boton" />
-          </Link>
+      <div style={buttonContainerStyle} className="buttonContainer">
+        <div className="button">
+          <img src="/img/page/tierran1.png" alt="cabeza" style={buttonStyle} />
         </div>
-      </section>
-    </main>
+        <Link href="/nivel1/mate/opeba">
+          <img src="/img/page/ob.png" alt="Opebacs" style={buttonStyle} className="button" />
+        </Link>
+        <Link href="/nivel1/mate/nudeyfra">
+          <img src="/img/page/NDYF.png" alt="Numdeyfra" style={buttonStyle} className="button" />
+        </Link>
+        <Link href="/nivel1/mate/geo">
+          <img src="/img/page/geo.png" alt="Geo" style={buttonStyle} className="button" />
+        </Link>
+        <Link href="/nivel1/mate/resodepro">
+          <img src="/img/page/rdp.png" alt="Resodepro" style={buttonStyle} className="button" />
+        </Link>
+      </div>
+    </>
   );
 }
+
