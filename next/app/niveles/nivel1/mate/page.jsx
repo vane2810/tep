d9525c2ebc5006d135bf877482a6699d9239df0d
vane2810 <olgavanessasf@gmail.com>
2024-05-19 18:35:11 -0600

    tep

--- next/app/niveles/nivel1/mate/page.jsx
+++ next/app/niveles/nivel1/mate/page.jsx
@@ -1,75 +1,41 @@
 import React from "react";
 import Link from 'next/link';
-
-
-// Definición del componente SeparadorRosa directamente en el mismo archivo
-const SeparadorRosa = () => (
-  <div className="separadorRosa"></div>
-);
-
-// Estilos para los botones definidos en el mismo archivo
-const buttonContainerStyle = {
-  display: 'flex',
-  justifyContent: 'center',
-  gap: '10px',
-  marginTop: '20px',
-};
-
-const buttonStyle = {
-  flex: '1 0 auto',
-  width: '250px',
-  height: 'auto',
-};
-
-const headerStyle = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  marginTop: '20px',
-  marginBottom: '20px',
-};
-
-const imageStyle = {
-  height: '250px',
-  width: 'auto',
-  marginRight: '360px',
-  marginLeft: '10px',
-};
-
-const paragraphStyle = {
-  color: 'black', // Cambia el color del texto a azul
-  fontFamily: 'Super', // Cambia la fuente del texto
-  fontSize: '76px',
-};
+import { SeparadorRosa } from "@/components/separador";
 
 export default function MatematicaPage() {
   return (
-    <>
-      <SeparadorRosa />
-      <div style={headerStyle}>
-        <img src="/img/niveles/mate/donkeysaludo.png" alt="Animated Image" style={imageStyle} />
-        <p style={paragraphStyle}>MATEMATICA</p>
-      </div>
-      <SeparadorRosa />
-      <div style={buttonContainerStyle} className="buttonContainer">
-        <div className="button">
-          <img src="/img/niveles/mate/cabeza.png" alt="cabeza" style={buttonStyle} />
+    <main>
+      {/* Bienvenida de Donkey*/}
+      <section>
+        <SeparadorRosa />
+        <div className="flex items-center justify-center mt-5 mb-5">
+          <img src="/img/niveles/mate/donkeysaludo.png" alt="Animated Image" className="h-64 w-auto mr-[360px] ml-2.5" />
+          <p className="text-black super text-[76px]">MATEMÁTICA</p>
+        </div>
+        <SeparadorRosa />
+      </section>
+
+      {/* Gusano*/}
+      <section>
+      <div className="flex justify-center gap-2.5 mt-5">
+        <div className="flex-auto w-64 h-auto">
+          <img src="/img/niveles/mate/cabeza.png" alt="cabeza" className="w-full h-auto" />
         </div>
         <Link href="/nivel1/mate/opeba">
-          <img src="/img/niveles/mate/ob.png" alt="Opebacs" style={buttonStyle} className="button" />
+          <img src="/img/niveles/mate/ob.png" alt="Opebacs" className="flex-auto w-64 h-auto" />
         </Link>
         <Link href="/nivel1/mate/nudeyfra">
-          <img src="/img/niveles/mate/NDYF.png" alt="Numdeyfra" style={buttonStyle} className="button" />
+          <img src="/img/niveles/mate/NDYF.png" alt="Numdeyfra" className="flex-auto w-64 h-auto" />
         </Link>
         <Link href="/nivel1/mate/geo">
-          <img src="/img/niveles/mate/geo.png" alt="Geog" style={buttonStyle} className="button" />
+          <img src="/img/niveles/mate/geo.png" alt="Geog" className="flex-auto w-64 h-auto" />
         </Link>
         <Link href="/nivel1/mate/resodepro">
-          <img src="/img/niveles/mate/rdp.png" alt="Resodepro" style={buttonStyle} className="button" />
+          <img src="/img/niveles/mate/rdp.png" alt="Resodepro" className="flex-auto w-64 h-auto" />
         </Link>
       </div>
-    </>
+      </section>
+      
+    </main>
   );
 }
-
-
