8cad405b52177aca181026acacc43bfffbbf2f68
Hazel2502 <hazelbgranados@gmail.com>
2024-06-09 11:30:49 -0600

    tep

--- next/components/minigame/lvl1/mate/suma/game4.jsx
+++ next/components/minigame/lvl1/mate/suma/game4.jsx
@@ -1,22 +1,8 @@
 "use client";
-
 import React, { useEffect, useState } from 'react';
 import Phaser from 'phaser';
 
 const Game4 = ({ updateScore = (f) => f, updateQuestionCount = (f) => f, questionCount = 0 }) => {
-  const [questions] = useState([
-    { image: 'mercury', options: ['Mercurio', 'Venus', 'Tierra', 'Marte'], correct: 'Mercurio' },
-    { image: 'venus', options: ['Júpiter', 'Venus', 'Saturno', 'Urano'], correct: 'Venus' },
-    { image: 'earth', options: ['Marte', 'Venus', 'Tierra', 'Neptuno'], correct: 'Tierra' },
-    { image: 'mars', options: ['Saturno', 'Marte', 'Mercurio', 'Júpiter'], correct: 'Marte' },
-    { image: 'jupiter', options: ['Saturno', 'Júpiter', 'Urano', 'Neptuno'], correct: 'Júpiter' },
-    { image: 'saturn', options: ['Urano', 'Neptuno', 'Saturno', 'Venus'], correct: 'Saturno' },
-    { image: 'uranus', options: ['Tierra', 'Urano', 'Marte', 'Júpiter'], correct: 'Urano' },
-    { image: 'neptune', options: ['Neptuno', 'Venus', 'Mercurio', 'Saturno'], correct: 'Neptuno' },
-    { image: 'sun', options: ['Sol', 'Luna', 'Estrella', 'Cometa'], correct: 'Sol' },
-    { image: 'moon', options: ['Planeta', 'Satélite', 'Estrella', 'Luna'], correct: 'Luna' }
-  ]);
-
   useEffect(() => {
     const config = {
       type: Phaser.AUTO,
@@ -32,22 +18,22 @@ const Game4 = ({ updateScore = (f) => f, updateQuestionCount = (f) => f, questio
     const game = new Phaser.Game(config);
 
     function preload() {
-      this.load.image('background', '/img/games/mate/ob/fondoji1.png');
-      this.load.image('mercury', '/img/games/mate/ob/mercurio.png');
+      this.load.image('fondo', '/img/games/mate/ob/fondoji1.png');
+      this.load.image('mercurio', '/img/games/mate/ob/mercurio.png');
       this.load.image('venus', '/img/games/mate/ob/venus.png');
-      this.load.image('earth', '/img/games/mate/ob/tierra.png');
-      this.load.image('mars', '/img/games/mate/ob/marte.png');
+      this.load.image('tierra', '/img/games/mate/ob/tierra.png');
+      this.load.image('marte', '/img/games/mate/ob/marte.png');
       this.load.image('jupiter', '/img/games/mate/ob/jupiter.png');
-      this.load.image('saturn', '/img/games/mate/ob/saturno.png');
-      this.load.image('uranus', '/img/games/mate/ob/urano.png');
-      this.load.image('neptune', '/img/games/mate/obe/neptuno.png');
-      this.load.image('sun', '/img/games/mate/ob/sol.png');
-      this.load.image('moon', '/img/games/mate/ob/luna.png');
-      this.load.image('next', '/img/games/mate/ob/flechitajuego.png'); // Imagen de la flecha
+      this.load.image('saturno', '/img/games/mate/ob/saturno.png');
+      this.load.image('urano', '/img/games/mate/ob/urano.png');
+      this.load.image('neptuno', '/img/games/mate/ob/neptuno.png');
+      this.load.image('sol', '/img/games/mate/ob/sol.png');
+      this.load.image('luna', '/img/games/mate/ob/luna.png');
+      this.load.image('flechita', '/img/games/mate/ob/flechitajuego.png');
     }
 
     function createScene() {
-      const background = this.add.image(340, 300, 'background');
+      const background = this.add.image(340, 300, 'fondo');
       background.setDisplaySize(680, 600);
       background.setAlpha(0.8);
 
@@ -74,97 +60,81 @@ const Game4 = ({ updateScore = (f) => f, updateQuestionCount = (f) => f, questio
       this.optionBox4 && this.optionBox4.destroy();
       this.scoreText && this.scoreText.destroy();
       this.nextButton && this.nextButton.destroy();
+      this.feedbackText && this.feedbackText.destroy();
 
-      this.planetImage = this.add.image(340, 150, currentQuestion.image);
+      this.planetImage = this.add.image(340, 250, currentQuestion.image);
       this.planetImage.setDisplaySize(200, 200);
 
-      this.questionText = this.add.text(340, 50, `Pregunta ${questionCount + 1}`, { fontSize: '24px', fill: '#ffffff' });
+      this.questionText = this.add.text(340, 50, `¡Conoces este planeta! ¿Cuál es?`, { fontSize: '24px', fill: '#ffffff' });
       this.questionText.setOrigin(0.5);
 
-      this.scoreText = this.add.text(50, 50, `Puntuación: ${updateScore}`, { fontSize: '20px', fill: '#ffffff' });
+      const options = Phaser.Math.RND.shuffle(currentQuestion.options);
 
-      this.optionBox1 = this.add.rectangle(200, 400, 100, 50, 0x76ADD0);
-      this.option1 = this.add.text(200, 400, currentQuestion.options[0], { fontSize: '24px', fill: '#ffffff' });
-      this.option1.setOrigin(0.5);
-      this.option1.setInteractive();
-      this.option1.on('pointerdown', () => checkAnswer.call(this, currentQuestion.options[0], currentQuestion.correct));
-      this.option1.on('pointerover', () => {
-        this.optionBox1.setScale(1.05);
-        this.optionBox1.setFillStyle(0xAD76D0);
-      });
-      this.option1.on('pointerout', () => {
-        this.optionBox1.setScale(1);
-        this.optionBox1.setFillStyle(0x76ADD0);
-      });
+      for (let i = 0; i < options.length; i++) {
+        const optionBox = this.add.rectangle(200 + i * 140, 450, 100, 50, 0x76ADD0);
+        const optionText = this.add.text(200 + i * 140, 450, options[i], { fontSize: '24px', fill: '#ffffff' });
+        optionText.setOrigin(0.5);
+        optionText.setInteractive();
+        optionText.on('pointerdown', () => checkAnswer.call(this, options[i], currentQuestion.correct));
+        optionText.on('pointerover', () => {
+          optionBox.setScale(1.05);
+          optionBox.setFillStyle(0xAD76D0);
+        });
+        optionText.on('pointerout', () => {
+          optionBox.setScale(1);
+          optionBox.setFillStyle(0x76ADD0);
+        });
+      }
 
-      this.optionBox2 = this.add.rectangle(340, 400, 100, 50, 0x76ADD0);
-      this.option2 = this.add.text(340, 400, currentQuestion.options[1], { fontSize: '24px', fill: '#ffffff' });
-      this.option2.setOrigin(0.5);
-      this.option2.setInteractive();
-      this.option2.on('pointerdown', () => checkAnswer.call(this, currentQuestion.options[1], currentQuestion.correct));
-      this.option2.on('pointerover', () => {
-        this.optionBox2.setScale(1.05);
-        this.optionBox2.setFillStyle(0xAD76D0);
-      });
-      this.option2.on('pointerout', () => {
-        this.optionBox2.setScale(1);
-        this.optionBox2.setFillStyle(0x76ADD0);
-      });
-
-      this.optionBox3 = this.add.rectangle(480, 400, 100, 50, 0x76ADD0);
-      this.option3 = this.add.text(480, 400, currentQuestion.options[2], { fontSize: '24px', fill: '#ffffff' });
-      this.option3.setOrigin(0.5);
-      this.option3.setInteractive();
-      this.option3.on('pointerdown', () => checkAnswer.call(this, currentQuestion.options[2], currentQuestion.correct));
-      this.option3.on('pointerover', () => {
-        this.optionBox3.setScale(1.05);
-        this.optionBox3.setFillStyle(0xAD76D0);
-      });
-      this.option3.on('pointerout', () => {
-        this.optionBox3.setScale(1);
-        this.optionBox3.setFillStyle(0x76ADD0);
-      });
-
-      this.optionBox4 = this.add.rectangle(340, 500, 100, 50, 0x76ADD0);
-      this.option4 = this.add.text(340, 500, currentQuestion.options[3], { fontSize: '24px', fill: '#ffffff' });
-      this.option4.setOrigin(0.5);
-      this.option4.setInteractive();
-      this.option4.on('pointerdown', () => checkAnswer.call(this, currentQuestion.options[3], currentQuestion.correct));
-      this.option4.on('pointerover', () => {
-        this.optionBox4.setScale(1.05);
-        this.optionBox4.setFillStyle(0xAD76D0);
-      });
-      this.option4.on('pointerout', () => {
-        this.optionBox4.setScale(1);
-        this.optionBox4.setFillStyle(0x76ADD0);
-      });
-
-      this.nextButton = this.add.image(640, 550, 'next');
+      this.nextButton = this.add.image(640, 550, 'flechita');
+      this.nextButton.setDisplaySize(50, 50);
+      this.nextButton.setVisible(false);
       this.nextButton.setInteractive();
-      this.nextButton.on('pointerdown', () => nextQuestion.call(this));
+      this.nextButton.on('pointerdown', () => {
+        changePlanetImage.call(this);
+        nextQuestion.call(this);
+      });
+    }
+
+    function changePlanetImage() {
+      this.planetImage.setTexture(Phaser.Math.RND.pick(['mercurio', 'venus', 'tierra', 'marte', 'jupiter', 'saturno', 'urano', 'neptuno', 'sol', 'luna']));
     }
 
     function checkAnswer(selectedAnswer, correctAnswer) {
+      this.feedbackText && this.feedbackText.destroy();
       if (selectedAnswer === correctAnswer) {
+        this.feedbackText = this.add.text(340, 350, '¡Es correcta!', { fontSize: '24px', fill: '#00ff00' }).setOrigin(0.5);
         updateScore(score => score + 1);
+      } else {
+        this.feedbackText = this.add.text(340, 350, '¡Es incorrecta!', { fontSize: '24px', fill: '#ff0000' }).setOrigin(0.5);
       }
-      updateQuestionCount(count => count + 1);
       this.nextButton.setVisible(true);
     }
 
     function nextQuestion() {
+      this.feedbackText && this.feedbackText.destroy();
       this.nextButton.setVisible(false);
-      if (questionCount < 9) {
-        createNewQuestion.call(this);
-      } else {
-        this.add.text(340, 300, '¡Juego terminado!', { fontSize: '32px', fill: '#ffffff' }).setOrigin(0.5);
-      }
+      updateQuestionCount(count => count + 1);
+      createNewQuestion.call(this);
     }
 
     return () => {
       game.destroy(true);
     };
-  }, [questionCount, updateScore, updateQuestionCount, questions]);
+  }, [questionCount, updateScore, updateQuestionCount]);
+
+  const [questions] = useState([
+    { image: 'mercurio', options: ['Mercurio', 'Venus', 'Tierra', 'Marte'], correct: 'Mercurio' },
+    { image: 'venus', options: ['Júpiter', 'Venus', 'Saturno', 'Urano'], correct: 'Venus' },
+    { image: 'tierra', options: ['Marte', 'Venus', 'Tierra', 'Neptuno'], correct: 'Tierra' },
+    { image: 'marte', options: ['Saturno', 'Marte', 'Mercurio', 'Júpiter'], correct: 'Marte' },
+    { image: 'jupiter', options: ['Saturno', 'Júpiter', 'Urano', 'Neptuno'], correct: 'Júpiter' },
+    { image: 'saturno', options: ['Urano', 'Neptuno', 'Saturno', 'Tierra'], correct: 'Saturno' },
+    { image: 'urano', options: ['Urano', 'Neptuno', 'Tierra', 'Marte'], correct: 'Urano' },
+    { image: 'neptuno', options: ['Urano', 'Neptuno', 'Venus', 'Marte'], correct: 'Neptuno' },
+    { image: 'sol', options: ['Saturno', 'Sol', 'Mercurio', 'Júpiter'], correct: 'Sol' },
+    { image: 'luna', options: ['Tierra', 'Luna', 'Venus', 'Marte'], correct: 'Luna' },
+  ]);
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh' }}>
@@ -174,4 +144,5 @@ const Game4 = ({ updateScore = (f) => f, updateQuestionCount = (f) => f, questio
 };
 
 export default Game4;
+
 
