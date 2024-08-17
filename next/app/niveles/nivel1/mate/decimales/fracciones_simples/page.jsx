1229489bdcd640ebdfdce7cb5e7b27569149583e
Hazel2502 <hazelbgranados@gmail.com>
2024-08-16 20:19:56 -0600

    tep

--- next/app/niveles/nivel1/mate/decimales/fracciones_simples/page.jsx
+++ next/app/niveles/nivel1/mate/decimales/fracciones_simples/page.jsx
@@ -22,7 +22,7 @@ export default function SumaPage() {
           </div>
           <div className="flex flex-col items-center justify-center mb-5 text-center">
             <div className="flex items-center justify-center">
-              <img src="/img/niveles/mate/compafig.png" alt="Animated Image" className="h-64 w-auto mr-4" />
+              <img src="/img/niveles/mate/figfrasim.png" alt="Animated Image" className="h-64 w-auto mr-4" />
               <p className="text-black super text-[40px] max-w-lg">SUMA Y RESTA DE NUMEROS DECIMALES</p>
             </div>
           </div>
@@ -36,7 +36,7 @@ export default function SumaPage() {
           <h2 className="text-2xl font-semibold text-center">Paso 1</h2>
           <h2 className="text-xl font-semibold text-center">¿Qué es una fracción?</h2>
           <p className="mt-2 text-black text-center">Una fracción representa una parte de un todo. Se compone de dos números: el numerador (arriba) y el denominador (abajo). Por ejemplo, en la fracción 1/4, el "1" es el numerador y el "4" es el denominador.</p>
-          <img src="/img/niveles/mate/paso1compa.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <img src="/img/niveles/mate/paso1frasim.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
       
@@ -46,7 +46,7 @@ export default function SumaPage() {
           <h2 className="text-2xl font-semibold text-center">Paso 2</h2>
           <h2 className="text-xl font-semibold text-center">Entendiendo el numerador y el denominador:</h2>
           <p className="mt-2 text-black text-center">El numerador indica cuántas partes estamos considerando, mientras que el denominador nos dice en cuántas partes iguales se ha dividido el todo. En 1/4, el "4" nos dice que el todo se ha dividido en 4 partes iguales, y el "1" nos dice que estamos considerando una de esas partes.</p>
-          <img src="/img/niveles/mate/paso2compa.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <img src="/img/niveles/mate/paso2frasim.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
 
@@ -56,7 +56,7 @@ export default function SumaPage() {
           <h2 className="text-2xl font-semibold text-center">Paso 3</h2>
           <h2 className="text-xl font-semibold text-center">Representación gráfica de las fracciones:</h2>
           <p className="mt-2 text-black text-center">Podemos representar gráficamente una fracción usando diagramas, como un círculo o un rectángulo dividido en partes iguales. Por ejemplo, para representar 1/2, podemos dibujar un círculo y dividirlo en 2 partes iguales, y luego sombrear una de ellas.</p>
-          <img src="/img/niveles/mate/paso3compa.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <img src="/img/niveles/mate/paso3frasim.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
 
@@ -66,7 +66,7 @@ export default function SumaPage() {
           <h2 className="text-2xl font-semibold text-center">Paso 4</h2>
           <h2 className="text-xl font-semibold text-center">Comparando fracciones:</h2>
           <p className="mt-2 text-black text-center">Al comparar fracciones con el mismo denominador, podemos ver cuál es mayor o menor observando los numeradores. Por ejemplo, 3/4 es mayor que 1/4 porque 3 partes de un todo son más que 1 parte.</p>
-          <img src="/img/niveles/mate/paso4compa.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <img src="/img/niveles/mate/paso4frasim.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
 
@@ -76,7 +76,7 @@ export default function SumaPage() {
           <h2 className="text-2xl font-semibold text-center">Paso 5</h2>
           <h2 className="text-xl font-semibold text-center">Práctica de representación gráfica:</h2>
           <p className="mt-2 text-black text-center">Ahora es tu turno. Practica dibujando y sombreando fracciones como 2/3 o 3/5. Esto te ayudará a visualizar mejor cómo se dividen y representan las fracciones. ¡Sigue practicando para dominar el concepto de fracciones simples!</p>
-          <img src="/img/niveles/mate/paso5compa.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <img src="/img/niveles/mate/paso5frasim.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
 
@@ -90,8 +90,8 @@ export default function SumaPage() {
       <Modal
         isOpen={modalOpen}
         onClose={() => setModalOpen(false)}
-        subjectName="Suma y resta de decimales"
-        continueLink="/niveles/nivel1/mate/decimales/comparacion/juegos"
+        subjectName="Fracciones simples"
+        continueLink="/niveles/nivel1/mate/decimales/fracciones_simples/juegos"
       />
     </main>
   );
--- next/app/niveles/nivel1/mate/decimales/suma_resta/page.jsx
+++ next/app/niveles/nivel1/mate/decimales/suma_resta/page.jsx
@@ -22,7 +22,7 @@ export default function SumaPage() {
           </div>
           <div className="flex flex-col items-center justify-center mb-5 text-center">
             <div className="flex items-center justify-center">
-              <img src="/img/niveles/mate/compafig.png" alt="Animated Image" className="h-64 w-auto mr-4" />
+              <img src="/img/niveles/mate/figsumres.jpg" alt="Animated Image" className="h-64 w-auto mr-4" />
               <p className="text-black super text-[40px] max-w-lg">SUMA Y RESTA DE NUMEROS DECIMALES</p>
             </div>
           </div>
@@ -36,7 +36,7 @@ export default function SumaPage() {
           <h2 className="text-2xl font-semibold text-center">Paso 1</h2>
           <h2 className="text-xl font-semibold text-center">Alineación de los números decimales:</h2>
           <p className="mt-2 text-black text-center">Para sumar o restar números decimales, el primer paso es alinear los números por el punto decimal. Esto asegura que estemos sumando o restando las partes correctas del número (las unidades con las unidades, las décimas con las décimas, etc.).</p>
-          <img src="/img/niveles/mate/paso1compa.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <img src="/img/niveles/mate/paso1sumres.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
       
@@ -46,7 +46,7 @@ export default function SumaPage() {
           <h2 className="text-2xl font-semibold text-center">Paso 2</h2>
           <h2 className="text-xl font-semibold text-center">Completar con ceros si es necesario:</h2>
           <p className="mt-2 text-black text-center">Si los números tienen una cantidad diferente de dígitos después del punto decimal, puedes añadir ceros para que tengan la misma longitud. Esto no cambia el valor del número, pero facilita la operación. Por ejemplo, al sumar 3.4 y 2.35, podemos escribir 3.40 para que ambos tengan dos dígitos después del punto decimal.</p>
-          <img src="/img/niveles/mate/paso2compa.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <img src="/img/niveles/mate/paso2sumres.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
 
@@ -56,7 +56,7 @@ export default function SumaPage() {
           <h2 className="text-2xl font-semibold text-center">Paso 3</h2>
           <h2 className="text-xl font-semibold text-center">Suma de números decimales:</h2>
           <p className="mt-2 text-black text-center">Para sumar, comenzamos desde el extremo derecho (el dígito más a la derecha) y sumamos como si fueran números enteros, moviéndonos hacia la izquierda. Asegúrate de mantener el punto decimal en la misma posición en la respuesta.</p>
-          <img src="/img/niveles/mate/paso3compa.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <img src="/img/niveles/mate/paso3sumres.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
 
@@ -65,8 +65,8 @@ export default function SumaPage() {
         <div className="flex flex-col items-center">
           <h2 className="text-2xl font-semibold text-center">Paso 4</h2>
           <h2 className="text-xl font-semibold text-center">Resta de números decimales:</h2>
-          <p className="mt-2 text-black text-center">Si el primer decimal es igual, comparamos el siguiente decimal. Por ejemplo, al comparar 5.342 y 5.348, comparamos decimal a decimal hasta encontrar una diferencia. Aquí, el tercer decimal (2 en 5.342 y 8 en 5.348) nos dice que 5.348 es mayor.</p>
-          <img src="/img/niveles/mate/paso4compa.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <p className="mt-2 text-black text-center">Para la resta, también operamos de derecha a izquierda. Si el dígito superior es menor que el inferior, pedimos prestado de la columna anterior. Por ejemplo:</p>
+          <img src="/img/niveles/mate/paso4sumres.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
 
@@ -76,7 +76,7 @@ export default function SumaPage() {
           <h2 className="text-2xl font-semibold text-center">Paso 5</h2>
           <h2 className="text-xl font-semibold text-center">Práctica de suma y resta de decimales</h2>
           <p className="mt-2 text-black text-center">Ahora es tu turno. Practica con estos ejemplos: 6.3 + 4.27 y 8.95 - 2.25. Alinea, suma o resta, y verifica tu respuesta. ¡La práctica constante te ayudará a dominar la suma y resta de números decimales!</p>
-          <img src="/img/niveles/mate/paso5compa.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <img src="/img/niveles/mate/paso5sumres.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
 
@@ -91,7 +91,7 @@ export default function SumaPage() {
         isOpen={modalOpen}
         onClose={() => setModalOpen(false)}
         subjectName="Suma y resta de decimales"
-        continueLink="/niveles/nivel1/mate/decimales/comparacion/juegos"
+        continueLink="/niveles/nivel1/mate/decimales/suma_resta/juegos"
       />
     </main>
   );
