74e6c670e175b8f709422125ff43f756d3dd30ba
Hazel2502 <hazelbgranados@gmail.com>
2024-08-18 22:56:58 -0600

    tep

--- next/app/niveles/nivel2/mate/decimales/conversion/page.jsx
+++ next/app/niveles/nivel2/mate/decimales/conversion/page.jsx
@@ -24,7 +24,7 @@ export default function ConversionPage() {
           <div className="flex flex-col items-center justify-center mb-5 text-center">
             <div className="flex items-center justify-center">
               <img src="/img/niveles/mate/figsime.png" alt="Animated Image" className="h-64 w-auto mr-4" />
-              <p className="text-black super text-[40px] max-w-lg">SIMETRÍA</p>
+              <p className="text-black super text-[40px] max-w-lg">CONVERSIÓN ENTRE FRACCIONES Y DECIMALES</p>
             </div>
           </div>
         </div>
@@ -34,48 +34,53 @@ export default function ConversionPage() {
       {/* paso 1 */}
       <section className="my-10 px-4">
         <div className="flex flex-col items-center">
-          <h2 className="text-2xl font-semibold text-center">¿Qué es la simetría?</h2>
-          <p className="mt-2 text-black text-center">La simetría es cuando una figura o un objeto se puede dividir en dos partes iguales que son exactamente iguales, como si fueran reflejos en un espejo.</p>
-          <img src="/img/niveles/mate/paso1sime.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <h2 className="text-2xl font-semibold text-center">¿Qué es una fracción?</h2>
+          <p className="mt-2 text-black text-center">Una fracción representa una parte de un todo. Por ejemplo, 1/2 significa que tienes una mitad de algo.</p>
+          <img src="/img/niveles/mate/paso1companum.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
       
       {/* paso 2 */}
       <section className="my-10 px-4">
         <div className="flex flex-col items-center">
-          <h2 className="text-2xl font-semibold text-center">Simetría vertical</h2>
-          <p className="mt-2 text-black text-center">Ocurre cuando puedes dibujar una línea de arriba hacia abajo en una figura, y las dos mitades a cada lado de la línea son iguales. Ejemplo: una mariposa.</p>
-          <img src="/img/niveles/mate/paso2sime.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <h2 className="text-2xl font-semibold text-center">¿Qué es un número decimal?</h2>
+          <p className="mt-2 text-black text-center">Un número decimal también representa partes de un todo, pero se escribe con un punto decimal. Por ejemplo, 0.5 es lo mismo que 1/2.</p>
+          <img src="/img/niveles/mate/paso2companum.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
 
       {/* paso 3 */}
       <section className="my-10 px-4">
         <div className="flex flex-col items-center">
-          <h2 className="text-2xl font-semibold text-center">Simetría horizontal</h2>
-          <p className="mt-2 text-black text-center">Ocurre cuando puedes dibujar una línea de lado a lado en una figura, y las dos mitades encima y debajo de la línea son iguales. Ejemplo: un lago que refleja las montañas.</p>
-          <img src="/img/niveles/mate/paso3sime.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <h2 className="text-2xl font-semibold text-center">Convertir una fracción a decimal: Paso 1</h2>
+          <p className="mt-2 text-black text-center">Para convertir una fracción a un decimal, divide el número de arriba (numerador) entre el número de abajo (denominador). Ejemplo: 1/4 → 1 ÷ 4 = 0.25.</p>
         </div>
       </section>
 
       {/* paso 4 */}
       <section className="my-10 px-4">
         <div className="flex flex-col items-center">
-          <h2 className="text-2xl font-semibold text-center">Simetría rotacional</h2>
-          <p className="mt-2 text-black text-center">Ocurre cuando puedes girar una figura alrededor de un punto central, y en algún momento, la figura parece igual a como era antes de girarla. Ejemplo: una estrella de mar.</p>
-          <img src="/img/niveles/mate/paso4sime.png" alt="Suma" className="h-32 w-auto mt-4" />
+          <h2 className="text-2xl font-semibold text-center">Convertir una fracción a decimal: Paso 2</h2>
+          <p className="mt-2 text-black text-center">Escribe el resultado de la división como un número decimal. Así, 1/4 se convierte en 0.25.</p>
         </div>
       </section>
 
       {/* paso 5 */}
       <section className="my-10 px-4">
         <div className="flex flex-col items-center">
-          <h2 className="text-2xl font-semibold text-center">Importancia de la simetría</h2>
+          <h2 className="text-2xl font-semibold text-center">Convertir un decimal a fracción: Paso 1</h2>
           <p className="mt-2 text-black text-center">La simetría ayuda a ver el equilibrio y la armonía en las formas que nos rodean. Es útil en el arte, la naturaleza, y hace que las cosas se vean bonitas y organizadas.</p>
-          <img src="/img/niveles/mate/paso5sime.png" alt="Suma" className="h-32 w-auto mt-4" />
         </div>
       </section>
 
+     {/* paso 6 */}
+     <section className="my-10 px-4">
+        <div className="flex flex-col items-center">
+          <h2 className="text-2xl font-semibold text-center">Simplificar la fracción</h2>
+          <p className="mt-2 text-black text-center">Simplifica la fracción dividiendo el numerador y el denominador por su máximo común divisor. Ejemplo: 75/100 se simplifica a 3/4.</p>
+          <img src="/img/niveles/mate/paso5sime.png" alt="Suma" className="h-32 w-auto mt-4" />
+        </div>
+      </section>
     
       {/* Botón para abrir el modal */}
       <div className="flex justify-end">
@@ -87,8 +92,8 @@ export default function ConversionPage() {
       <Modal
         isOpen={modalOpen}
         onClose={() => setModalOpen(false)}
-        subjectName="Simetría"
-        continueLink="/niveles/nivel1/mate/geometria/simetria/juegos"
+        subjectName="Conversión entre fracciones y números decimales"
+        continueLink="/niveles/nivel2/mate/decimales/conversion/juegos"
       />
     </main>
   );
