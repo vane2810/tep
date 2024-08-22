08b81e2bba9f9b8a4f94b3f74c6f427fb08ef310
vane2810 <olgavanessasf@gmail.com>
2024-08-22 07:48:01 -0600

    tep

---
+++ next/app/games/lvl1/mate/geometria/figuras/game1/page.jsx
@@ -0,0 +1,133 @@
+// Juego 1 - Figuras- Nivel 1
+"use client";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Game1Modal from '@/components/modals/games/mate/geometria/game1Modal';
+import dynamic from 'next/dynamic';
+import { SeparadorVerde } from "@/components/separador";
+import Typewriter from "@/components/typeWriter";
+
+// Importación del juego
+const Game1 = dynamic(() => import('@/components/minigame/lvl1/mate/geometria/figuras/game1'), { ssr: false });
+
+const GamePage1 = () => {
+  const [gameStarted, setGameStarted] = useState(false);
+  const [showInstructions, setShowInstructions] = useState(false);
+  const [feedback, setFeedback] = useState('');
+  const [score, setScore] = useState(0);
+  const [currentScene, setCurrentScene] = useState(1);
+  const [gameKey, setGameKey] = useState(0);
+
+  const toggleInstructions = () => {
+    setShowInstructions(!showInstructions);
+  };
+
+  const startGame = () => {
+    setGameStarted(true);
+    setFeedback('');
+    setScore(0);
+    setCurrentScene(1);
+    setGameKey(prevKey => prevKey + 1);
+  };
+
+  const updateFeedback = (newFeedback, color) => {
+    setFeedback(<span style={{ color }}>{newFeedback}</span>);
+  };
+
+  const updateScore = (points) => {
+    setScore(prevScore => prevScore + points);
+  };
+
+  const proceedToNextScene = () => {
+    if (currentScene < 5) {
+      setCurrentScene(currentScene + 1);
+      setGameKey(prevKey => prevKey + 1); // Reiniciar el componente para la nueva escena
+    } else {
+      if (score < 60) {
+        setFeedback('No alcanzaste el puntaje necesario. Debes volver a intentarlo.');
+      } else {
+        setFeedback('¡Felicidades! Has completado todas las escenas.');
+      }
+      setGameStarted(false); // Detener el juego
+    }
+  };
+
+  const restartGame = () => {
+    setGameStarted(true);
+    setFeedback('');
+    setScore(0);
+    setCurrentScene(1);
+    setGameKey(prevKey => prevKey + 1); // Reiniciar el componente para la nueva escena
+  };
+
+  return (
+    <main className="bg-gray-100">
+      <SeparadorVerde />
+      <div className="flex items-center justify-between flex-wrap">
+        {/* Botón de Volver */}
+        <div className="ml-8 inline-block mb-20">
+          <Link href="/niveles/nivel1/mate/geometria/figuras/juegos">
+            <img src="/img/home/regresar.png" alt="Volver" className="w-10 h-auto" title="Volver a la página anterior" />
+          </Link>
+        </div>
+        {/* Contenedor del Typewriter, la imagen y el botón */}
+        <div className="flex items-center my-6 mx-auto">
+          {/* Imagen */}
+          <div className="flex-shrink-0 mr-4">
+            <img src="/img/niveles/mate/introfig.png" alt="Decimales" className="h-40 w-auto" />
+          </div>
+          {/* Typewriter y botón */}
+          <div className="flex flex-col">
+            {/* Texto */}
+            <div className="story font-bold text-xl mb-4">
+              <Typewriter
+                text="   Lee las indicaciones para comenzar"
+                speed={40}
+              />
+            </div>
+            {/* Botón de Indicaciones */}
+            <button className="verde story text-xl text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
+              onClick={toggleInstructions}> Indicaciones
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Modal de Indicaciones */}
+      <Game1Modal
+        show={showInstructions}
+        onClose={toggleInstructions}
+        onStartGame={startGame}
+        imageUrl="/img/niveles/mate/introfig.png"
+        subtitle=" Identificación de Decimales"
+      />
+
+      {/* Escena del juego */}
+      {gameStarted && (
+        <section className='min-h-screen flex flex-col items-center'>
+          <div className="my-16 p-6 story bg-white rounded-lg shadow-lg w-[850px]">
+            <h1 className="text-3xl font-bold mb-4 text-center"> Identificación de Decimales</h1>
+            <Game1 
+              key={gameKey} 
+              updateFeedback={updateFeedback} 
+              updateScore={updateScore} 
+              proceedToNextScene={proceedToNextScene} 
+              isFinalScene={currentScene === 5} // Indica si es la última escena
+              finalScore={score} // Pasar el puntaje final
+              restartGame={restartGame} // Función para reiniciar el juego
+            />
+            <div className="mt-8">
+              <p className="text-xl font-semibold">Ejercicio {currentScene}</p>
+              <p className="text-xl font-semibold">Feedback: {feedback}</p>
+              <p className="text-xl font-semibold">Estrellas: {score}</p>
+            </div>
+          </div>
+        </section>
+      )}
+
+      <SeparadorVerde />
+    </main>
+  );
+};
+
+export default GamePage1;
---
+++ next/app/games/lvl1/mate/geometria/figuras/game2/page.jsx
@@ -0,0 +1,144 @@
+// Juego 2 - Intro de deicmales - Nivel 1
+"use client";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Game1Modal from '@/components/modals/games/mate/decimales/game2Modal';
+import dynamic from 'next/dynamic';
+import { SeparadorVerde } from "@/components/separador";
+import Typewriter from "@/components/typeWriter";
+
+// Importación del juego
+const Game2 = dynamic(() => import('@/components/minigame/lvl1/mate/decimales/intro/game2'), { ssr: false });
+
+const GamePage2 = () => {
+  const [gameStarted, setGameStarted] = useState(false);
+  const [showInstructions, setShowInstructions] = useState(false);
+  const [feedback, setFeedback] = useState('');
+  const [score, setScore] = useState(0);
+  const [correctCount, setCorrectCount] = useState(0); // Estado para contar las respuestas correctas
+  const [showRetryButton, setShowRetryButton] = useState(false); // Estado para mostrar botón de "Volver a Intentar"
+
+  const toggleInstructions = () => {
+    setShowInstructions(!showInstructions);
+  };
+
+  const startGame = () => {
+    setGameStarted(true);
+    setFeedback('');
+    setScore(0);
+    setCorrectCount(0); // Reiniciar el conteo de respuestas correctas al comenzar el juego
+    setShowRetryButton(false); // Ocultar botón de "Volver a Intentar"
+  };
+
+  const updateFeedback = (newFeedback, color) => {
+    setFeedback(<span style={{ color }}>{newFeedback}</span>);
+  };
+
+  const updateScore = (newScore) => {
+    setScore(newScore); // Actualizar directamente al nuevo valor, ya que cada respuesta correcta es 20 puntos
+  };
+
+  const incrementCorrectCount = () => {
+    setCorrectCount((prevCount) => prevCount + 1);
+  };
+
+  const finalizeGame = (finalScore) => {
+    if (finalScore >= 80) {
+      setFeedback("¡Felicidades! Has completado el juego.");
+    } else {
+      setFeedback("No has alcanzado la puntuación necesaria.");
+      setShowRetryButton(true); // Mostrar botón de "Volver a Intentar" si no alcanzó 80 puntos
+    }
+  };
+
+  const resetGame = () => {
+    // Reiniciar la escena de Phaser
+    const gameContainer = document.getElementById('game-container');
+    if (gameContainer) {
+      while (gameContainer.firstChild) {
+        gameContainer.removeChild(gameContainer.firstChild);
+      }
+    }
+    setGameStarted(false);
+    startGame(); // Reiniciar el juego
+  };
+
+  return (
+    <main className="bg-gray-100">
+      <SeparadorVerde />
+      <div className="flex items-center justify-between flex-wrap">
+        {/* Botón de Volver */}
+        <div className="ml-8 inline-block mb-20">
+          <Link href="/niveles/nivel1/mate/decimales/intro/juegos">
+            <img src="/img/home/regresar.png" alt="Volver" className="w-10 h-auto" title="Volver a la página anterior" />
+          </Link>
+        </div>
+        {/* Contenedor del Typewriter, la imagen y el botón */}
+        <div className="flex items-center my-6 mx-auto">
+          {/* Imagen */}
+          <div className="flex-shrink-0 mr-4">
+            <img src="/img/niveles/mate/introfig.png" alt="Decimales" className="h-40 w-auto" />
+          </div>
+          {/* Typewriter y botón */}
+          <div className="flex flex-col">
+            {/* Texto */}
+            <div className="story font-bold text-xl mb-4">
+              <Typewriter
+                text="   Lee las indicaciones para comenzar"
+                speed={40}
+              />
+            </div>
+            {/* Botón de Indicaciones */}
+            <button className="verde story text-xl text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-300"
+              onClick={toggleInstructions}> Indicaciones
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* Modal de Indicaciones */}
+      <Game1Modal
+        show={showInstructions}
+        onClose={toggleInstructions}
+        onStartGame={startGame}
+        imageUrl="/img/niveles/mate/introfig.png"
+        subtitle="¿Cuál es mayor?"
+      />
+
+      {/* Escena del juego */}
+      {gameStarted && (
+        <section className='min-h-screen flex flex-col items-center'>
+          <div className="my-16 p-6 story bg-white rounded-lg shadow-lg w-[850px]">
+            <h1 className="text-3xl font-bold mb-4 text-center">¿Cuál es mayor?</h1>
+            <Game2
+              updateFeedback={updateFeedback} 
+              updateScore={setScore}  // Actualizar la puntuación directamente
+              finalizeGame={finalizeGame} 
+              incrementCorrectCount={incrementCorrectCount} // Pasar la función para incrementar respuestas correctas
+              resetGame={resetGame} // Pasar la función para reiniciar el juego
+            />
+            <div className="mt-8">
+              <p className="text-xl font-semibold">Feedback: {feedback}</p>
+              <p className="text-xl font-semibold">Estrellas: {score} / 100</p> {/* Actualización del total a 100 */}
+              <p className="text-xl font-semibold">Respuestas correctas: {correctCount} de 5</p> {/* Nuevo apartado */}
+            </div>
+
+            {/* Botón "Volver a Intentar" */}
+            {showRetryButton && (
+              <button 
+                className="mt-4 verde story text-xl text-white py-2 px-4 rounded hover:bg-red-700 transition duration-300"
+                onClick={resetGame}
+              >
+                Volver a Intentar
+              </button>
+            )}
+          </div>
+        </section>
+      )}
+
+      <SeparadorVerde />
+    </main>
+  );
+};
+
+export default GamePage2;
