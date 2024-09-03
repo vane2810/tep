739c881657129f97cb07d72e034d2b232745f807
Hazel2502 <hazelbgranados@gmail.com>
2024-09-03 09:50:29 -0600

    tep

--- next/app/niveles/nivel2/page.jsx
+++ next/app/niveles/nivel2/page.jsx
@@ -1,49 +1,144 @@
-// Inicio Nivel 2
-import React from "react";
+// Página de inicio del NIVEL 2
+"use client";
+import React, { useState, useEffect } from "react";
+import Volver from "@/components/botonVolver";
 import Link from 'next/link';
-import Volver from '@/components/botonVolver'
-import { SeparadorAnaranjado, SeparadorRojo } from "@/components/separador";
+import { SeparadorAmarillo, SeparadorRosa, SeparadorAzul } from "@/components/separador";
+import useSession from '@/hooks/useSession';
+import Typewriter from "@/components/typeWriter";
+import '@/styles/animacion.css';
 
-export default function Nivel2() {
-  return (
+export default function Nivel1() {
+  const [isVerifying, setIsVerifying] = useState(true);
+  const [showWelcome, setShowWelcome] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const { session } = useSession();
+
+  const fullText = "  ¡Hola, pequeños genios! Bienvenidos al Nivel 2  ¡Vamos a aprender y a jugar!";
+  const images = [
+    "/img/personajes/niveles/bienvenida/munditok1.png",
+    "/img/personajes/niveles/bienvenida/munditok2.png",
+    "/img/personajes/niveles/bienvenida/munditok3.png"
+  ];
+
+  useEffect(() => {
+    if (session) {
+      const hasSeenWelcome = localStorage.getItem('hasSeenWelcome');
+
+      if (!hasSeenWelcome) {
+        setShowWelcome(true);
+        const timeout = setTimeout(() => {
+          localStorage.setItem('hasSeenWelcome', 'true');
+          setShowWelcome(false);
+          setIsVerifying(false);
+        }, 10000);
+
+        const imageInterval = setInterval(() => {
+          setCurrentImageIndex(prev => (prev + 1) % images.length);
+        }, 4000);
+
+        return () => {
+          clearTimeout(timeout);
+          clearInterval(imageInterval);
+        };
+      } else {
+        setIsVerifying(false);
+      }
+    } else {
+      setIsVerifying(false);
+    }
+  }, [session]);
+
+  if (isVerifying) {
+    return null;
+  }
+
+  return showWelcome ? (
+    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-purple-900 via-indigo-900 to-black text-white">
+      <div className="text-center">
+        <img
+          src={images[currentImageIndex]}
+          alt="Welcome"
+          className="h-64 w-auto mb-4 mx-auto"
+        />
+        <div className="text-3xl font-bold story">
+          <Typewriter text={fullText} speed={80} />
+        </div>
+
+      </div>
+    </div>
+  ) : (
     <main>
-      <SeparadorRojo />
+      <SeparadorAzul/>
       <section>
         {/* Volver */}
-        <Volver href="/" title="Volver a la página principal" />
-        <div className="mt-6 ml-10 flex justify-center items-center">
-          <img src="/img/personajes/niveles/marten2.png" alt="Nivel 2" className="boton w-40 md:w-48 lg:w-56 xl:w-64 mx-2" />
-        </div>
-        
-          
+        {!session && (
+          <Volver href="/" title="Volver a la página de inicio" />
+        )}
+        <div className="w-full flex flex-col md:flex-row items-center justify-center p-4">
+          {/* Celestia */}
+          <div className="w-full md:w-3/4 flex items-center justify-center p-4">
+            <div className="flex items-center">
+              <img
+                src="/img/personajes/niveles/bienvenida/munditok1.png"
+                alt="Nivel 1"
+                className="h-64 w-auto animate-tambaleo mr-10"
+              />
+              <p className="text-black super text-3xl md:text-3xl text-left">
+                ¡BIENVENIDOS AL NIVEL II! <br /> PLANETA KAORI
+              </p>
+            </div>
+          </div>
 
+          {/* Juego Introductorio */}
+          <div className="w-full md:w-1/4 flex flex-col justify-center items-center p-4 mt-8 md:mt-0">
+            <Link href="/games/lvl2/intro">
+              <img
+                src="/img/home/juego_intro.png"
+                alt="Juego Introductorio"
+                className="w-36 md:w-44 lg:w-52 xl:w-60  animate-float"
+              />
+            </Link>
+          </div>
+        </div>
       </section>
 
-      <SeparadorRojo />
-      <section>
-        {/* Botones*/}
+      <SeparadorAzul/>
+      <section className="mt-10">
         <div className="flex justify-center items-center mb-10">
-          <Link href="/niveles/nivel2/mate">
-            <img src="/img/personajes/donkey/donkeyboton.png" alt="Matematica" className="boton w-40 md:w-48 lg:w-56 xl:w-64 mx-2" />
+          <Link href="/niveles/nivel1/mate">
+            <img
+              src="/img/personajes/donkey/donkeyboton.png"
+              alt="Matematica"
+              className="boton w-40 md:w-48 lg:w-56 xl:w-64 mx-4 rounded-full transition-transform transform hover:scale-105 hover:shadow-lg"
+            />
           </Link>
-          <Link href="/niveles/nivel2/lenguaje">
-            <img src="/img/personajes/principe/principeboton.png" alt="Lenguaje" className="boton w-40 md:w-48 lg:w-56 xl:w-64 mx-2" />
+        
+          <Link href="/niveles/nivel1/lenguaje">
+            <img
+              src="/img/personajes/principe/principeboton.png"
+              alt="Lenguaje"
+              className="boton w-40 md:w-48 lg:w-56 xl:w-64 mx-4 rounded-full transition-transform transform hover:scale-105 hover:shadow-lg"
+            />
           </Link>
-          <Link href="/niveles/nivel2/ingles">
-            <img src="/img/personajes/griffit/griffitboton.png" alt="Ingles" className="boton w-40 md:w-48 lg:w-56 xl:w-64 mx-2" />
+          <Link href="/niveles/nivel1/ingles">
+            <img
+              src="/img/personajes/griffit/griffitboton.png"
+              alt="Ingles"
+              className="boton w-40 md:w-48 lg:w-56 xl:w-64 mx-4 rounded-full transition-transform transform hover:scale-105 hover:shadow-lg "
+            />
           </Link>
-          <Link href="/niveles/nivel2/sociales">
-            <img src="/img/personajes/burbuja/burbujaboton.png" alt="Sociales" className="boton w-40 md:w-48 lg:w-56 xl:w-64 mx-2" />
+          <Link href="/niveles/nivel1/sociales">
+            <img
+              src="/img/personajes/burbuja/burbujaboton.png"
+              alt="Sociales"
+              className="boton w-40 md:w-48 lg:w-56 xl:w-64 mx-4 rounded-full transition-transform transform hover:scale-105 hover:shadow-lg"
+            />
           </Link>
         </div>
       </section>
+      <SeparadorAzul/>
 
-      <div className="flex justify-center items-center my-10">
-        <Link
-          href="/games/lvl2/intro"
-          className=" w-40 h-20 md:w-50 lg:w-60 bg-purple-500 text-white text-center font-bold py-4 px-6 rounded-lg shadow-lg hover:bg-purple-700"
-        >Juego Introductorio</Link>
-      </div>
     </main>
   );
 }
