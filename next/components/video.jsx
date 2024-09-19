a77547de1960921c8cce987faedbb51a8c6c73c0
vane2810 <olgavanessasf@gmail.com>
2024-09-18 18:07:40 -0600

    tep

--- next/components/video.jsx
+++ next/components/video.jsx
@@ -1,3 +1,4 @@
+// Botones de inicio para cada nivel
 import React from "react";
 import Link from 'next/link';
 
@@ -9,28 +10,40 @@ export default function Video() {
                 <source src="/img/home/galaxia.mp4" type="video/mp4" />
             </video>
             {/* Botones */}
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col md:flex-row md:gap-8 items-center z-10">
+            <div className="top-1/2 left-1/2 z-10 absolute flex md:flex-row flex-col items-center md:gap-8 transform -translate-x-1/2 -translate-y-1/2">
                 <div className="relative">
                     <Link href="/niveles/nivel1">
-                        <img src="/img/personajes/niveles/tierran1.png" alt="Nivel 1" className="boton w-full md:w-auto lg:w-auto lg:max-w-xs" />
+                        <img
+                          src="/img/personajes/niveles/tierran1.png"
+                          alt="Nivel 1"
+                          className="hover:shadow-lg w-full md:w-auto lg:w-auto lg:max-w-xs transition-transform duration-200 boton hover:scale-105"
+                        />
                     </Link>
-                    <div className="absolute top-0 left-1/2 transform -translate-y-full -translate-x-1/2 text-black font-bold p-2 rounded whitespace-nowrap" style={{ backgroundColor: '#ED1566' }}>
+                    <div className="top-0 left-1/2 absolute p-2 rounded font-bold text-black whitespace-nowrap transform -translate-x-1/2 -translate-y-full rosado">
                         Nivel 1, Cuarto Grado
                     </div>
                 </div>
                 <div className="relative">
                     <Link href="/niveles/nivel2">
-                        <img src="/img/personajes/niveles/marten2.png" alt="Nivel 2" className="boton w-full md:w-auto lg:w-auto lg:max-w-xs" />
+                        <img
+                          src="/img/personajes/niveles/marten2.png"
+                          alt="Nivel 2"
+                          className="hover:shadow-lg w-full md:w-auto lg:w-auto lg:max-w-xs transition-transform duration-200 boton hover:scale-105"
+                        />
                     </Link>
-                    <div className="absolute top-0 left-1/2 transform -translate-y-full -translate-x-1/2 text-black font-bold p-2 rounded whitespace-nowrap" style={{ backgroundColor: '#ED1566' }}>
+                    <div className="top-0 left-1/2 absolute p-2 rounded font-bold text-black whitespace-nowrap transform -translate-x-1/2 -translate-y-full rosado">
                         Nivel 2, Quinto Grado
                     </div>
                 </div>
                 <div className="relative">
                     <Link href="/niveles/nivel3">
-                        <img src="/img/personajes/niveles/jupitern3.png" alt="Nivel 3" className="boton w-full md:w-auto lg:w-auto lg:max-w-xs" />
+                        <img
+                          src="/img/personajes/niveles/jupitern3.png"
+                          alt="Nivel 3"
+                          className="hover:shadow-lg w-full md:w-auto lg:w-auto lg:max-w-xs transition-transform duration-200 boton hover:scale-105"
+                        />
                     </Link>
-                    <div className="absolute top-0 left-1/2 transform -translate-y-full -translate-x-1/2 text-black font-bold p-2 rounded whitespace-nowrap" style={{ backgroundColor: '#ED1566' }}>
+                    <div className="top-0 left-1/2 absolute p-2 rounded font-bold text-black whitespace-nowrap transform -translate-x-1/2 -translate-y-full rosado" >
                         Nivel 3, Sexto Grado
                     </div>
                 </div>
@@ -38,5 +51,3 @@ export default function Video() {
         </div>
     );
 }
-
-
