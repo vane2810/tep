c6099c35f411e8d4263e6048ca8f53b05670abfd
vane2810 <olgavanessasf@gmail.com>
2024-10-28 08:19:48 -0600

    tep

--- next/app/niveles/nivel1/mate/page.jsx
+++ next/app/niveles/nivel1/mate/page.jsx
@@ -14,29 +14,28 @@ export default function MatematicaPage() {
       <section>
         <SeparadorRosa />
         {/* Volver */}
-        <div className="mt-6 ml-10 inline-block">
+        <div className="inline-block mt-6 ml-10">
           <Link href="/niveles/nivel1">
             <img src="/img/home/regresar.png" alt="Volver" className="w-10 h-auto" title="Volver a la página anterior" />
           </Link>
         </div>
-        <div className="flex flex-col md:flex-row items-center justify-center mb-5">
-          <div className="flex flex-col items-center md:mr-8 md:ml-2.5 mb-4 md:mb-0">
-            <img src="/img/personajes/donkey/donkeysaludo.png" alt="Donkey" className="h-64 w-auto mx-2.5 md:mr-8 md:ml-2.5" />
+        <div className="flex md:flex-row flex-col justify-center items-center mb-5">
+          <div className="flex flex-col items-center md:mr-8 mb-4 md:mb-0 md:ml-2.5">
+            <img src="/img/personajes/donkey/donkey.png" alt="Donkey" className="mx-2.5 md:mr-8 md:ml-2.5 w-auto h-64" />
           </div>
-          <p className="text-black super text-4xl md:text-6xl md:ml-8">MATEMÁTICA</p>
+          <p className="md:ml-8 text-4xl text-black md:text-6xl super">MATEMÁTICA</p>
         </div>
-        <SeparadorRosa />
       </section>
 
       {/*Gusano */}
       <section>
-        <div className="flex justify-center overflow-x-auto mt-10">
+        <div className="flex justify-center mt-10 overflow-x-auto">
           {/* Cabeza */}
           <div className="flex items-center mt-[-30px] md:mt-[-50px] lg:mt-[-80px]">
             <img
               src="/img/niveles/mate/nivel1/cabeza.png"
               alt="cabeza"
-              className="worm-segment w-40 h-auto mt-4 lg:w-60 md:mr-2.5 animate-gusano"
+              className="worm-segment mt-4 md:mr-2.5 w-40 lg:w-60 h-auto animate-gusano"
             />
           </div>
           {/* Operaciones básicas */}
@@ -44,7 +43,7 @@ export default function MatematicaPage() {
             <img
               src="/img/niveles/mate/nivel1/ob.png"
               alt="Operaciones básicas"
-              className="worm-segment flex-auto w-40 h-auto mt-8 mb-4 lg:w-60 md:mr-2.5 animate-gusano"
+              className="flex-auto worm-segment mt-8 md:mr-2.5 mb-4 w-40 lg:w-60 h-auto animate-gusano"
             />
           </Link>
           {/* Decimales*/}
@@ -52,7 +51,7 @@ export default function MatematicaPage() {
             <img
               src="/img/niveles/mate/nivel1/NDYF.png"
               alt="Números decimales"
-              className="worm-segment flex-auto w-40 h-auto mb-4 lg:w-60 md:mr-2.5 animate-gusano"
+              className="flex-auto worm-segment md:mr-2.5 mb-4 w-40 lg:w-60 h-auto animate-gusano"
             />
           </Link>
           {/* Geometría*/}
@@ -60,7 +59,7 @@ export default function MatematicaPage() {
             <img
               src="/img/niveles/mate/nivel1/geo.png"
               alt="Geometría"
-              className="worm-segment flex-auto w-40 h-auto mb-4 mt-8 lg:w-60 md:mr-2.5 animate-gusano"
+              className="flex-auto worm-segment mt-8 md:mr-2.5 mb-4 w-40 lg:w-60 h-auto animate-gusano"
             />
           </Link>
           {/* Resolución de problemas (juegos finales) */}
@@ -68,11 +67,14 @@ export default function MatematicaPage() {
             <img
               src="/img/niveles/mate/nivel1/refuerzof.png"
               alt="Resolución de problemas"
-              className="worm-segment flex-auto w-40 h-auto mb-4 md:w-50 lg:w-60 animate-gusano"
+              className="flex-auto worm-segment mb-4 w-40 md:w-50 lg:w-60 h-auto animate-gusano"
             />
           </Link>
         </div>
       </section>
+      
+      <SeparadorRosa />
     </main>
+    
   );
 }
