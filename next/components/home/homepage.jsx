a03021e10975f964f2d435e8f04ffe366307051d
vane2810 <olgavanessasf@gmail.com>
2024-11-11 16:17:55 -0600

    tep

--- next/components/home/homepage.jsx
+++ next/components/home/homepage.jsx
@@ -1,50 +1,51 @@
-// Botones de inicio para cada nivel
 import React from "react";
 import Link from 'next/link';
 
 export default function Video() {
     return (
         <div className="relative w-full overflow-hidden">
-            {/* Video */}
-            <video autoPlay loop muted className="w-full h-auto md:h-96 object-cover">
+            {/* Video de fondo */}
+            <video autoPlay loop muted className="w-full h-auto object-cover max-h-[60vh]">
                 <source src="/img/home/galaxia.mp4" type="video/mp4" />
             </video>
-            {/* Botones */}
-            <div className="top-1/2 left-1/2 z-10 absolute flex md:flex-row flex-col items-center md:gap-8 transform -translate-x-1/2 -translate-y-1/2">
-                <div className="relative">
-                    <Link href="/niveles/nivel1">
-                        <img
-                          src="/img/personajes/niveles/tierran1.png"
-                          alt="Nivel 1"
-                          className="hover:shadow-lg w-full md:w-auto lg:w-auto lg:max-w-xs transition-transform duration-200 boton hover:scale-105"
-                        />
-                    </Link>
-                    <div className="top-0 left-1/2 absolute p-2 rounded font-bold text-black whitespace-nowrap transform -translate-x-1/2 -translate-y-full rosado">
-                        Nivel 1, Cuarto Grado
+            {/* Contenedor de botones distribuido horizontalmente */}
+            <div className="absolute inset-0 flex items-center justify-center w-full p-4">
+                <div className="flex flex-row items-center justify-around w-full max-w-7xl gap-8">
+                    <div className="flex flex-col items-center w-56 sm:w-64 md:w-72 lg:w-80 xl:w-[36rem]">
+                        <div className="mt-40 mb-25 bg- rosado text-black font-bold rounded px-4 py-1 text-center text-base">
+                            Nivel 1, Cuarto Grado
+                        </div>
+                        <Link href="/niveles/nivel1">
+                            <img
+                                src="/img/personajes/niveles/tierran1.png"
+                                alt="Nivel 1"
+                                className="hover:shadow-lg w-full transition-transform duration-200 hover:scale-105"
+                            />
+                        </Link>
                     </div>
-                </div>
-                <div className="relative">
-                    <Link href="/niveles/nivel2">
-                        <img
-                          src="/img/personajes/niveles/marten2.png"
-                          alt="Nivel 2"
-                          className="hover:shadow-lg w-full md:w-auto lg:w-auto lg:max-w-xs transition-transform duration-200 boton hover:scale-105"
-                        />
-                    </Link>
-                    <div className="top-0 left-1/2 absolute p-2 rounded font-bold text-black whitespace-nowrap transform -translate-x-1/2 -translate-y-full rosado">
-                        Nivel 2, Quinto Grado
+                    <div className="flex flex-col items-center w-56 sm:w-64 md:w-72 lg:w-80 xl:w-[36rem]">
+                        <div className="mt-40 mb-25 bg- rosado text-black font-bold rounded px-4 py-1 text-center text-base">
+                            Nivel 2, Quinto Grado
+                        </div>
+                        <Link href="/niveles/niveles/nivel2">
+                            <img
+                                src="/img/personajes/niveles/marten2.png"
+                                alt="Nivel 2"
+                                className="hover:shadow-lg w-full transition-transform duration-200 hover:scale-105"
+                            />
+                        </Link>
                     </div>
-                </div>
-                <div className="relative">
-                    <Link href="/niveles/nivel3">
-                        <img
-                          src="/img/personajes/niveles/jupitern3.png"
-                          alt="Nivel 3"
-                          className="hover:shadow-lg w-full md:w-auto lg:w-auto lg:max-w-xs transition-transform duration-200 boton hover:scale-105"
-                        />
-                    </Link>
-                    <div className="top-0 left-1/2 absolute p-2 rounded font-bold text-black whitespace-nowrap transform -translate-x-1/2 -translate-y-full rosado" >
-                        Nivel 3, Sexto Grado
+                    <div className="flex flex-col items-center w-56 sm:w-64 md:w-72 lg:w-80 xl:w-[36rem]">
+                        <div className="mt-45 mb-30 bg- rosado text-black font-bold rounded px-4 py-1 text-center text-base">
+                            Nivel 3, Sexto Grado
+                        </div>
+                        <Link href="/niveles/niveles/nivel3">
+                            <img
+                                src="/img/personajes/niveles/jupitern3.png"
+                                alt="Nivel 3"
+                                className="hover:shadow-lg w-full transition-transform duration-200 hover:scale-105"
+                            />
+                        </Link>
                     </div>
                 </div>
             </div>
